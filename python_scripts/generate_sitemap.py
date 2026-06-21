9a2f8702f780b823f64b19394c3bb73f67fdceb9
gradient01 <gradient01.dev@gmail.com>
2026-06-21 15:23:36 +0200

    Change sitemap.xml and generate_sitemap.py

--- python_scripts/generate_sitemap.py
+++ python_scripts/generate_sitemap.py
@@ -1,27 +1,14 @@
 from xml.etree.ElementTree import Element, SubElement, ElementTree, indent
 import json
+import os
 
 def generate_sitemap():
 
-    urls = [
-        "https://pynumstore.github.io/pynumstore/",
-        "https://pynumstore.github.io/pynumstore/index.html",
-        "https://pynumstore.github.io/pynumstore/search.html",
-        "https://pynumstore.github.io/pynumstore/forcreators.html",
-        "https://pynumstore.github.io/pynumstore/404.html"
-    ]
+    urls = []
 
-    with open("data/scripts_index.json", "r") as f:
-        scripts = json.load(f)
-    for script in scripts:
-        urls.append(f"https://pynumstore.github.io/pynumstore/script.html?creator={script['creator']}&name={script['name']}")
-    del scripts
-    
-    with open("data/creators_index.json", "r") as f:
-        creators = json.load(f)
-    for creator in creators:
-        urls.append(f"https://pynumstore.github.io/pynumstore/creator.html?name={creator}")
-    del creators
+    for file in os.listdir():
+        if file.endswith(".html") and file != "404.html":
+            urls.append(f"https://pynumstore.github.io/pynumstore/{file}")
 
     urlset = Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
     for url in urls:
